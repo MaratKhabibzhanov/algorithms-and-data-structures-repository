96791be1c858fd8890f24d87ac47917dc2184b57
riddick <riddick@mail.com>
2022-06-04 22:21:26 +0500

    editing OnSort

--- OnSort.py
+++ OnSort.py
@@ -1,6 +1,14 @@
-def counting_sort(list):
-    pass
+def counting_sort(in_list, notation):
+    frequency = [0] * notation
+    out_list = list()
+    for i in in_list:
+        frequency[i] += 1
+    for digit in range(notation):
+        count = frequency[digit]
+        for _ in range(count):
+            out_list.append(digit)
+    return out_list
 
 
-counting_list = [2, 6, 8, 9, 3, 6, 7, 5, 6, 8, 2]
-print(counting_sort(counting_list))
+counting_list = [2, 6, 8, 9, 3, 6, 7, 5, 6, 8, 2, 6, 4, 3, 6, 9, 0, 1, 2, 6, 3]
+print(*counting_sort(counting_list, 10))
